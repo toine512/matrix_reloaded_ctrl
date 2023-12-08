45fb2da49d10f0396a82021e2b16d10aa9d3c840
Antoine <toine512@users.noreply.github.com>
2023-12-08 23:34:28 +0100

    Adds the possibility to use UPX with generate_exe.bat
    using the command line.
    First argument to the Batch file is the directory containing UPX executable as PyInstaller's --upx-dir requires.
    Else --noupx is passed to PyInstaller.

--- windows/generate_exe.bat
+++ windows/generate_exe.bat
@@ -1,8 +1,16 @@
 @echo off
 rem OEM 850, CR LF
+rem setlocal EnableDelayedExpansion
+rem Handle UPX
+if [%1]==[] (
+	set "upx_arg=--noupx"
+) else (
+	set "upx_arg=--upx-dir %1"
+)
+rem Run
 < NUL call make.bat
 call matrix_controller\py\venv\Scripts\activate.bat
 echo on
 pip install -U pyinstaller
-pyinstaller --onefile matrix_controller\py\matrix_display.py
+pyinstaller %upx_arg% --onefile matrix_controller\py\matrix_display.py
 @pause
